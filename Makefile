99393a213dcc489532f6e066eea441f2e2fdfc2f
Tobias <tobias@Ovidius>
2024-11-23 22:17:41 +0100

    Added deprecated OpenSSL AES implementation, it works but is basically just the demo file

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 GCC = gcc
-GCCFLAGS = -O3
+GCCFLAGS = -O3 -g
+
 
 TARGETS = \
 			pp
@@ -10,12 +11,23 @@ HEADERS = \
 LIBS    = \
 			-lmastik
 
+help:
+	@echo "Use":
+	@echo ""
+	@echo "$$ make all" 
+	@echo ""
+	@echo "to compile all attacks (requires the OpenSSL crypto library), or"
+	@echo ""
+	@echo "$$ make <attack1> <attack2> ... <attack3>"
+	@echo ""
+	@echo "to compile the specific attacks you want."
+
+
 
 all:		$(TARGETS)
 
 pp:			pp.c
-				$(GCC) $(GCCFLAGS) -o $@ $^ $(LIBS)
-
+				$(GCC) $(GCCFLAGS) -o $@ $^ $(LIBS) -lcrypto
 
 clean:
 		rm -f $(TARGETS) *.o
--- pp.c
+++ pp.c
@@ -6,6 +6,7 @@
 
 #include <mastik/util.h>
 #include <mastik/synctrace.h>
+#include <openssl/aes.h>
 
 #define AES_SIZE 16
 
@@ -34,16 +35,20 @@ void analyse(int64_t data[256][1024], int *key, int *offset) {
 
 void crypto(uint8_t *input, uint8_t *output, void *data)
 {
-
+    AES_KEY *aeskey = (AES_KEY *)data;
+    AES_encrypt(input, output, aeskey);
 }
 
 int main(int argc, char** argv)
 {
     const uint8_t key[] = 
     {
-        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+        0x0f, 0x1e, 0x2d, 0x3c, 0x4b, 0x5a, 0x69, 0x78,
+        0x87, 0x96, 0xa5, 0xb4, 0xc3, 0xd2, 0xe1, 0xf0
     };
+
+    AES_KEY aeskey;
+    AES_set_encrypt_key(key, 128, &aeskey);
 
 
     delayloop(1000000000);
@@ -54,7 +59,7 @@ int main(int argc, char** argv)
                                             NULL,
                                             NULL,
                                             crypto,
-                                            NULL,
+                                            &aeskey,
                                             0xf0,
                                             1);
 
@@ -64,8 +69,8 @@ int main(int argc, char** argv)
         int guess, offset;
         printf("Key byte %2d: ", i);
         analyse(clusters[i].avg, &guess, &offset);
-        printf(" %.2x", guess);
-        printf("   ");
+        printf(" %1x", guess);
+        printf("    ");
         printf(" %.2x", key[i]);
         printf("\n");
     }
